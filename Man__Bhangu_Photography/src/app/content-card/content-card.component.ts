70ea5c472709037129baf34a8a413f8b90e2f87a
Varinder Kaur <VK92@myscc.ca>
2023-02-03 19:41:13 -0500

    updated files

--- Man__Bhangu_Photography/src/app/content-card/content-card.component.ts
+++ Man__Bhangu_Photography/src/app/content-card/content-card.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//import { Content } from '../helper-files/content-interface';
+import { Content } from '../helper-files/content-interface';
 import { ContentList } from '../helper-files/content-list';
 
 @Component({
@@ -10,40 +10,7 @@ import { ContentList } from '../helper-files/content-list';
 export class ContentCardComponent {
   myContentList : ContentList = new ContentList();
 
- /* contentItem1 : Content = {
-    id: 0,
-    title: 'Manchahat',
-    description: 'This is a description',
-    creator: 'Creator'
-  }
-  contentItem2 : Content = {
-    id: 0,
-    title: 'Nothing',
-    description: 'This is a description2',
-    creator: 'Creator1'
-  }
-  contentItem3 : Content = {
-    id: 0,
-    title: 'Nothing',
-    description: 'This is a description4',
-    creator: 'Creator2'
-  }
-
-  constructor(){
-    this.myContentList.addFunction(this.contentItem1);
-    this.myContentList.addFunction(this.contentItem2);
-    this.myContentList.addFunction(this.contentItem3);
-  }
-
-  displayMyIndex(index : number) {
-    let myItems = this.myContentList.getItems();
-
-    if (myItems.length > 0 && index > 0 && index < myItems.length){
-      return this.myContentList.printIndex(index);
-    } else {
-      
-    }
-  }*/
+ 
   ngOnInit(){
     
   }
